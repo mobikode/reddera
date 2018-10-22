1e0bf22070c5e22d86c0b04bc719387ab1b7fa2c
mobikode <ajit@mobikode.com>
2018-10-22 21:48:39 +0530

    New changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,6 +5,7 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.time_zone = 'Asia/Kolkata'
   config.cache_classes = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
@@ -30,11 +31,14 @@ Rails.application.configure do
   Paperclip.options[:command_path] = "/usr/bin/identify"
 
   config.eager_load = false
-  config.action_controller.asset_host = 'http://18.136.194.81/'
+  config.action_controller.asset_host = 'http://localhost:3000'
   config.action_mailer.asset_host = config.action_controller.asset_host
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
+  config.serve_static_assets = false
+  config.assets.compress = true
+
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
@@ -59,7 +63,7 @@ Rails.application.configure do
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
-  config.assets.raise_runtime_errors = true
+  #config.assets.raise_runtime_errors = true
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
